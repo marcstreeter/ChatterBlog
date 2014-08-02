dee8a77929b05244aadcde6ae891d376dbbbafe9
Marc Streeter <mstreeter@gmail.com>
2014-08-02 18:18:44 -0500

    added register() for registering new users

--- app/member/controllers.py
+++ app/member/controllers.py
@@ -7,13 +7,15 @@ from app.config import config
 # Library
 from app.shared.response import jsonified
 from app.shared import error
+from app.shared.Member import Member
 
 # 3rd Party
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
 
 logger = logging.getLogger(config.root_logger + __name__)
 
 member_blueprint = Blueprint('member', __name__, url_prefix=config.api_prefix + "member")
+
 
 @member_blueprint.route('/', methods=['GET'])
 def show():
@@ -21,3 +23,25 @@ def show():
         return jsonified("members on the way")
     except:
         raise error.SystemError()
+
+
+@member_blueprint.route('/', methods=["POST"])
+def register():
+    try:
+        json_resp = request.get_json(force=True)
+        password = json_resp.get("password")
+        username = json_resp.get("username")
+        email = json_resp.get("email")
+        phone = json_resp.get("phone")
+
+        member = Member(name=username, email=email, phone=phone, password=password)
+
+    except:
+        raise error.SystemError()
+
+    try:
+        return jsonified(data="registered user %s" % member)
+
+    except:
+        raise error.SystemError()
+
